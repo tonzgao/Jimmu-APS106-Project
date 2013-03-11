6aeda437a3ede32314474bfa94c68f570e5db403
tonzgao <tonyzgao@gmail.com>
2013-03-10 23:34:30 -0400

    from apache to mit

--- Play.c
+++ Play.c
@@ -39,7 +39,7 @@ int play(void)
 	scanf("%d", &mode)
 	switch (mode) {
 		case 1: {
-					timestamp(); 
+					timestamp();
 				    for (warn = 0; sizex > 39 || sizey > 39 || sizex < 2 || sizey < 2; warn++) {
 				        if (warn > 0) {
 				            printf("Sorry, both x and y have to be between 2 and 39\n\n");
@@ -50,7 +50,7 @@ int play(void)
 				        scanf("%d", &sizey);
 				    }
 					char grid[sizex][sizey];
-					generate_grid(grid); 
+					generate_grid(grid);
 					break;
 				}
 		case 2: {
@@ -64,7 +64,7 @@ int play(void)
                     	fclose(input);
                     	warn = read_grid(grid, path);
                 	}
-                	break; 
+                	break;
             	}
 		case 3: timestamp(); printf("Enter size"); scanf("%d%d", &sizex, &sizey); grid = generate_grid(sizex, sizey); ai_play(); play(); break;
 		case 4: timestamp(); printf("Path pls"); scanf("%s", &path); grid = read_grid(path); ai_play(); play(); break;
@@ -283,16 +283,14 @@ void ai_play(void)
 
 
 //  COPYRIGHT LICENCE
-//  Copyright [2013] [Gary Cai, Anthony Gao, and Jimmy Tieu]
+//   The MIT License (MIT)
 //
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
+//Copyright (c) <2013> <Zipeng Cai, Anthony Gao, Richard Shangguan, Jimmy Tieu>
 //
-//       http://www.apache.org/licenses/LICENSE-2.0
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//
+//IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
